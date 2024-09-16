82de78e6b9a7b1c89c16de712324b38d39807171
Vitor Mozer <vitor.mozer@atsinformatica.com.br>
2024-09-16 12:43:54 -0300

    Erro view

--- Novo Sistema/Controller/UPessoaController.pas
+++ Novo Sistema/Controller/UPessoaController.pas
@@ -11,16 +11,32 @@ type
        function GravaPessoa(
                    pPessoa : TPessoa) : Boolean;
 
+       function RetornaCondicaoPessoa(pID_Pessoa : Integer) : String;
 
+     published
+        class function getInstancia : TPessoaController;
   end;
 
 implementation
+
+uses UPessoaDAO;
+
+var
+  _instance : TPessoaController;
 
 { TPessoaController }
 
 constructor TPessoaController.Create;
 begin
    inherited Create;
+end;
+
+class function TPessoaController.getInstancia: TPessoaController;
+begin
+   if _instance = nil then
+      _instance := TPessoaController.Create;
+
+   Result := _instance;
 end;
 
 function TPessoaController.GravaPessoa(pPessoa: TPessoa): Boolean;
@@ -34,16 +50,48 @@ begin
 
         Result := False;
 
-        xPessoaDAO :=
+        xPessoaDAO :=    // aq estou passando a conexao do banco que estava na UpessoaDao para o objeto xPessoaDAO
            TPessoaDAO.Create(TConexao.get.getConn);
 
+        if pPessoa.Id = 0 then
+        begin
+           xPessoaDAO.Insere(pPessoa);
+        end
+        else
+        begin
+           xPessoaDAO.Atualiza(pPessoa, RetornaCondicaoPessoa(pPessoa.Id));
+        end;
+
+        TConexao.get.confirmaTransacao;
+
      finally
-
-     end;   
+        if (xPessoaDAO <> nil) then
+           FreeAndNil(xPessoaDAO);
+     end;
    except
+      on E : Exception do
+      begin
+         TConexao.get.cancelaTransacao;
+         Raise Exception.Create(
+            'Falha ao gravar os dados da pessoa [Controller]. '#13+
+            e.Message);
+      end;
 
-   end;  
+   end;
+end;
+
+function TPessoaController.RetornaCondicaoPessoa(
+  pID_Pessoa: Integer): String;
+var
+  xChave : String;
+begin
+   xChave := 'ID';
+
+
+   Result :=
+   'WHERE                                                '#13+
+   '   '+ xChave+ ' = '+ QuotedStr(IntToStr(pID_Pessoa))+' '#13; //função Quoted para substituir as aspas duplas / quadruplas
+
 end;
 
 end.
- 
